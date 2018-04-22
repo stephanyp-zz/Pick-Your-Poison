3a720de6d424f7f9294f9d91a29f5d3d57dc1d5d
StephSenpai <stephany.jacqueline@gmail.com>
2018-04-22 16:31:11 -0700

    java is hard

--- app.js
+++ app.js
@@ -1,31 +1,19 @@
-$.getJSON( "https://api.airtable.com/v0/appp3qUdwqW3M4BCV/Games?api_key=keybojDUtvSsJ4IGr", function( data ) {
- // console.log(data.records);
- var items = [];
-  $.each( data.records, function( index, val ) {
-    // console.log(val.fields["Name"])
-    console.log("what is rating from api?", val.fields["rating"])
-    items.push("<li id='" + val.id + "'>" + val.fields["Name"] + "</li>"
-             + "<img src='" + val.fields["Pictures"][0].url + "'>"
-               + "<p id=' " + val.id + "'>" + val.fields["Attachments"] + "</p>"
-             + "<h3 id=' " + val.id + "'>" + val.fields["Cost"] + "</h3>"
-    + "<h3 id=' " + val.id + "'>" + val.fields["Buy"] + "</h3>"
-             + "<h2 id=' " + val.id + "'>" + val.fields["rate"] + "</h2>"
-              );
-  });
-// if (val.fields['rating']){items += '<p>' + val.fields['rating'] + '</p>';});
-//data from airtable, check Github to see that stuff lol beacuse u only have so long to do this
-  $( "<ol/>", {
-    "class": "my-new-list",
-    html: items.join( "" )
-  }).appendTo( "body" );
-});
- var items = [];
-  $.each( data.records, function( index, val ) {
-    console.log(val.fields["pictures"])
-    items.push( "<li id='" + val.id + "'>" + val.fields["pictures"] + "</li>" );
-  });
+  $.getJSON( "https://api.airtable.com/v0/appp3qUdwqW3M4BCV/Games?api_key=keybojDUtvSsJ4IGr", function( data ) {
+    // console.log(data.records);
+    var items = [];
+    items.push(`<div class="row">`);
+    $.each( data.records, function( index, val ) {
+      console.log(val.fields)
+      var id = val.id;
+      var name = val.fields["Name"];
+      var Pictures = val.fields["Pictures"] ? val.fields["Pictures"][0].url : '';
+      var Attachments = val.fields["Attachments"];
+      var Cost = val.fields["Cost"];
+      var rate = val.fields["rate"];
+      var itemHTML = gameListItem(id, Name, Pictures,Attachments, rate);
+      items.push(itemHTML);
+    });
+    items.push(`</div>`);
 
-  $( "<ol/>", {
-    "class": "my-new-list",
-    html: items.join( "" )
-  }).appendTo( "body" );
+    $(".game-list" ).append(items.join(""));
+  });
