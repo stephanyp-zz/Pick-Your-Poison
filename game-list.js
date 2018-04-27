9e2480388eddb52dccc7acc8d3a299df2ae8b934
Stephany <stephany.jacqueline@gmail.com>
2018-04-26 18:10:03 -0700

    adding things

--- game-list.js
+++ game-list.js
@@ -15,11 +15,14 @@ var gameListItem = function(id, Name, Pictures,Attachments, Cost, rate, Buy) {
     </div>
   </div>`;
 }
-  $.getJSON( "https://api.airtable.com/v0/appp3qUdwqW3M4BCV/Games?api_key=keybojDUtvSsJ4IGr", function( data ) {
+
+console.log(window.location.search)
+
+  $.getJSON( "https://api.airtable.com/v0/appp3qUdwqW3M4BCV/Games/recXQa600rMtkcHXb", function( val ) {
     // console.log(data.records);
     var items = [];
     items.push(`<div class="row">`);
-    $.each( data.records, function( index, val ) {
+
       console.log(val.fields)
       var id = val.id;
       var name = val.fields["Name"];
@@ -30,7 +33,7 @@ var gameListItem = function(id, Name, Pictures,Attachments, Cost, rate, Buy) {
       var Buy = val.fields["Buy"];
       var itemHTML = gameListItem(id, Name, Pictures,Attachments, rate, Buy);
       items.push(itemHTML);
-    });
+
     items.push(`</div>`);
 
     $(".game-list" ).append(items.join(""));
